885c06c6a49c98a538342f716f805456ada1a65b
Luca-Mihail Ion <101120197+lucaion@users.noreply.github.com>
2022-06-12 21:47:59 +0300

    update2

--- ClothesResell2/app/src/main/java/com/example/clothesresell/Fragments/NotificationsFragment.java
+++ ClothesResell2/app/src/main/java/com/example/clothesresell/Fragments/NotificationsFragment.java
@@ -1,4 +1,4 @@
-package com.example.clothesresell.Fragments;
+package com.example.clothesresell.Fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,8 +6,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.clothesresell.Adapter.NotificationAdapter;
+import com.example.clothesresell.Model.Notification;
 import com.example.clothesresell.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -56,10 +67,46 @@ public class NotificationsFragment extends Fragment {
         }
     }
 
+    private RecyclerView recyclerView;
+    private NotificationAdapter notificationAdapter;
+    private List<Notification> notificationList;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        notificationList = new ArrayList<>();
+        notificationAdapter = new NotificationAdapter(getContext(), notificationList);
+        recyclerView.setAdapter(notificationAdapter);
+        readNotifications();
+
+        return view;
+    }
+
+    private void readNotifications() {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Notifications").child(firebaseUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot){
+                notificationList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Notification notification = snapshot.getValue(Notification.class);
+                    notificationList.add(notification);
+                }
+
+                Collections.reverse(notificationList);
+                notificationAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
