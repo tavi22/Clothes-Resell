5260002bbd7144c8ca659ccb3c57d92de0ffd8b1
Stefan Boboc <bobocstefan675@gmail.com>
2022-06-13 16:32:55 +0300

    UI Profile done

---
+++ ClothesResell2/app/src/main/java/com/example/clothesresell/GridImageAdaptor.java
@@ -0,0 +1,100 @@
+package com.example.clothesresell;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import java.util.ArrayList;
+
+public class GridImageAdaptor extends ArrayAdapter<String> {
+    private Context mContext;
+    private LayoutInflater mInflator;
+    private int layoutResource;
+    private String mAppend;
+    private ArrayList<String> imgURLs;
+
+    public GridImageAdaptor(Context mContext, int layoutResource, String mAppend, ArrayList<String> imgURLs) {
+        super(mContext, layoutResource, imgURLs);
+        this.mInflator = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.mContext = mContext;
+        this.layoutResource = layoutResource;
+        this.mAppend = mAppend;
+        this.imgURLs = imgURLs;
+    }
+
+    private static class ViewHolder {
+        ImageView image;
+        ProgressBar mProgressBar;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        /**
+         * Viewholder build pattern (does not load all images at once)
+         */
+
+        final ViewHolder holder;
+
+        if(convertView == null){
+            convertView = mInflator.inflate(layoutResource, parent, false);
+            holder = new ViewHolder();
+//            holder.mProgressBar = (ProgressBar) convertView.findViewById(R.id.profileProgre);
+            holder.image = (ImageView) convertView.findViewById(R.id.gridImageView);
+
+            convertView.setTag(holder);
+        }
+        else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        String imgURL = getItem(position);
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.displayImage(mAppend + imgURL, holder.image, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                if(holder.mProgressBar != null){
+//                    mProgressBar.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+//                if(mProgressBar != null){
+//                    mProgressBar.setVisibility(View.GONE);
+//                }
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+//                if(mProgressBar != null){
+//                    mProgressBar.setVisibility(View.GONE);
+//                }
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+//                if(mProgressBar != null){
+//                    mProgressBar.setVisibility(View.GONE);
+//                }
+            }
+        });
+
+
+        return convertView;
+    }
+}
