12f92d119fc1cd67033b7dc8822eea63c8eb3dd9
Octavian Mitrica <octavian.mitrica4@gmail.com>
2022-06-14 18:28:57 +0300

    small_details

--- ClothesResell2/app/src/main/java/com/example/clothesresell/MainActivity.java
+++ ClothesResell2/app/src/main/java/com/example/clothesresell/MainActivity.java
@@ -18,7 +18,6 @@ import com.example.clothesresell.Fragments.HelpFragment;
 import com.example.clothesresell.Fragments.HomeFragment;
 import com.example.clothesresell.Fragments.MyProfileFragment;
 import com.example.clothesresell.Fragments.NotificationsFragment;
-import com.example.clothesresell.Fragments.ProfileFragment;
 import com.example.clothesresell.Fragments.WishlistFragment;
 import com.example.clothesresell.databinding.ActivityMainBinding;
 import com.google.android.material.appbar.MaterialToolbar;
